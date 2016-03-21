54191528ef8becc979c8c46eea1a7f5d6f5f01b1
krystalrain <15727575905@163.com>
2016-03-21 18:30:20 +0800

    health

---
+++ js/firstindex.js
@@ -0,0 +1,14 @@
+  $(document).snowfall('clear');
+		$(document).snowfall({
+			 shadow : true, round : true,
+			  flakeCount:40,
+			  minSize: 5,
+			  maxSize:15}); 
+
+
+$("img.gb").click(function(){
+
+	$(".gray").show();//œ‘ æ
+	$("#Music").get(0).play();
+
+			});
